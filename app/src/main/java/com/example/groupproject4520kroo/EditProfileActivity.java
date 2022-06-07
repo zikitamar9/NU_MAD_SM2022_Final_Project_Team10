27d0d0f2fa213f629b5db4e1c153ccc8b6b52da0
Itamar Zik <zikitamar@gmail.com>
2022-06-06 20:51:12 -0400

    -"Committing Slides 8-11 to ZikBranch"

---
+++ app/src/main/java/com/example/groupproject4520kroo/EditProfileActivity.java
@@ -0,0 +1,14 @@
+package com.example.groupproject4520kroo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+    }
+}
